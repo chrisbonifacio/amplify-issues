59320b332a33738c0f6234fdd0e3572d5ed46ba0
Chris Bonifacio <christopher.bonifacio@gmail.com>
2021-08-20 00:24:25 -0400

    fix unescaped entity error

--- issue-8727-next-oauth/pages/404.js
+++ issue-8727-next-oauth/pages/404.js
@@ -3,7 +3,8 @@ export default function NotFound() {
   return (
     <div style={{ textAlign: "center", marginTop: "2em" }}>
       <Link style={{ color: "blue" }} href="/">
-        Sorry, this page wasn't found. Click here to return to the homepage.
+        Sorry, this page wasn&apos;t found. Click here to return to the
+        homepage.
       </Link>
     </div>
   );
