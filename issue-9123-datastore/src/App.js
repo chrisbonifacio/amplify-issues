e39eb2a8c141d985b5bbc9422b9ae8d1e8069cc6
Chris Bonifacio <christopher.bonifacio@gmail.com>
2021-10-28 16:11:58 -0400

    update repros

---
+++ issue-9123-datastore/src/App.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Amplify, DataStore } from "aws-amplify";
+import { Post } from "./models";
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
+
+const initialPost = {
+  title: "",
+};
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [newPost, setNewPost] = useState(initialPost);
+
+  const handleChange = (e) => {
+    setNewPost({
+      ...newPost,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const savePost = async (e) => {
+    e.preventDefault();
+    try {
+      await DataStore.save(new Post(newPost));
+      await fetchPosts();
+      setNewPost(initialPost);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchPosts = async () => {
+    try {
+      const res = await DataStore.query(Post);
+      setPosts(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  return (
+    <div className="App-header">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      <main>
+        <form onSubmit={savePost}>
+          <input
+            id="title"
+            name="title"
+            type="text"
+            placeholder="Title"
+            value={newPost.title}
+            onChange={handleChange}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+        {posts.map((post) => (
+          <p>{post.title}</p>
+        ))}
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ issue-9123-datastore/src/models/schema.js
@@ -0,0 +1,124 @@
+export const schema = {
+    "models": {
+        "Post": {
+            "name": "Post",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "title": {
+                    "name": "title",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "comments": {
+                    "name": "comments",
+                    "isArray": true,
+                    "type": {
+                        "model": "Comment"
+                    },
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true,
+                    "association": {
+                        "connectionType": "HAS_MANY",
+                        "associatedWith": "postID"
+                    }
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Posts",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                }
+            ]
+        },
+        "Comment": {
+            "name": "Comment",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "postID": {
+                    "name": "postID",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "content": {
+                    "name": "content",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Comments",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "key",
+                    "properties": {
+                        "name": "byPost",
+                        "fields": [
+                            "postID",
+                            "content"
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "ac84d00b8394fec38927ff12606b8837"
+};
